7ce027e5932ad13dbfd42d4489b0497f51e9f65b
beatificho <beatific@empal.com>
2015-02-05 13:10:31 +0900

    test

--- daram-web/src/main/java/org/beatific/daram/web/schedule/DummyJobBean.java
+++ daram-web/src/main/java/org/beatific/daram/web/schedule/DummyJobBean.java
@@ -5,22 +5,17 @@ import java.util.Date;
 import org.apache.commons.lang.math.RandomUtils;
 import org.beatific.daram.web.websocket.Message;
 import org.beatific.daram.web.websocket.OutputMessage;
-import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.messaging.core.MessageSendingOperations;
 import org.springframework.messaging.simp.broker.BrokerAvailabilityEvent;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-@Component
-public class DummyJobBean implements
-		ApplicationListener<BrokerAvailabilityEvent> {
+//@Component
+public class DummyJobBean implements ApplicationListener<BrokerAvailabilityEvent> {
 
 	private final MessageSendingOperations<String> messagingTemplate;
 
-	@Autowired
 	public DummyJobBean(final MessageSendingOperations<String> messagingTemplate) {
 		this.messagingTemplate = messagingTemplate;
 	}
